6e5f63b4d30af3632f6afadf3d7f4a6b972b976c
Gabriel Azubel <gabrielazubel63t@gmail.com>
2022-10-26 15:52:12 -0300

    estructura de carpetas y rutas principales

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Inicio from './components/views/Inicio';
+import Administrador from './components/views/Administrador';
+import DetalleReceta from './components/views/DetalleReceta';
+import Error404 from './components/views/Error404';
+import NavBar from './components/common/NavBar';
+import Footer from './components/common/Footer';
 
 function App() {
   return (
-    <div>
-      <h1>Blog de recetas</h1>
-    </div>
+    <BrowserRouter>
+      <NavBar/>
+      <Routes>
+        <Route exact path='/' element={<Inicio/>} />
+        <Route exact path='/administrar' element={<Administrador/>} />
+        <Route exact path='/detalle' element={<DetalleReceta/>} />
+        <Route exact path='*' element={<Error404/>} />
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/views/DetalleReceta.jsx
@@ -0,0 +1,9 @@
+const DetalleReceta = () => {
+    return (
+        <div>
+            <h1>Detalle receta</h1>
+        </div>
+    );
+};
+
+export default DetalleReceta;
