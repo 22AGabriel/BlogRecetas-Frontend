f9ab22baa41cd9da3646a7aceb3847877aaa3638
Gabriel Azubel <gabrielazubel63t@gmail.com>
2022-10-26 21:48:51 -0300

    se muestran las recetas en la grilla de Inicio

--- src/components/views/Inicio.jsx
+++ src/components/views/Inicio.jsx
@@ -1,17 +1,26 @@
+import { useEffect, useState } from "react";
 import { Container, Row } from "react-bootstrap";
+import { consultarAPI } from "../helpers/queries";
 import CardReceta from "./receta/CardReceta";
 
 const Inicio = () => {
+    const [recetas, setRecetas] = useState([]);
+
+  useEffect(() => {
+    consultarAPI().then((respuesta) => {
+      setRecetas(respuesta);
+    });
+  }, []);
+
     return (
         <Container className="my-5 mainSection">
             <div className="text-center mb-5">
               <h1>Blog de recetas</h1>
             </div>
             <Row>
-                <CardReceta></CardReceta>
-                <CardReceta></CardReceta>
-                <CardReceta></CardReceta>
-                <CardReceta></CardReceta>
+              {
+                recetas.map((receta) => (<CardReceta key={receta.id} receta={receta}></CardReceta>))
+              }
             </Row>
         </Container>
     );
--- src/components/views/receta/CardReceta.jsx
+++ src/components/views/receta/CardReceta.jsx
@@ -1,14 +1,16 @@
 import { Card, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-const CardReceta = () => {
+const CardReceta = ({receta}) => {
+  const {nombreReceta, imagen} = {...receta}
+
   return (
     <Col sm={12} md={4} lg={3} className="my-2 cardGrilla">
       <Link to={'/detalle'} className='text-decoration-none text-dark text-center'>
         <Card>
-          <Card.Img variant='top' src={'https://cwnoticias.com/download/multimedia.normal.8a73efa2d830f245.70726570697a7a615f6e6f726d616c2e6a7067.jpg'}></Card.Img>
+          <Card.Img variant='top' className="imgGrilla" src={imagen}></Card.Img>
           <Card.Body>
-            <Card.Title>Prepizza</Card.Title>
+            <Card.Title>{nombreReceta}</Card.Title>
           </Card.Body>
         </Card>
       </Link>
