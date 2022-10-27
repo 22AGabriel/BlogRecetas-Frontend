2d09969e6f1f9e3148698f32f45726e3e5ba6b3b
Gabriel Azubel <gabrielazubel63t@gmail.com>
2022-10-26 23:20:57 -0300

    maquetado de Error404

--- src/components/views/Error404.jsx
+++ src/components/views/Error404.jsx
@@ -2,8 +2,9 @@ import { Container } from "react-bootstrap";
 
 const Error404 = () => {
     return (
-        <Container className="my-5 mainSection">
-            <h1>Error 404</h1>
+        <Container className="my-5 mainSection d-flex flex-column align-items-center justify-content-center">
+            <img className="w-75" src="https://img.freepik.com/vector-gratis/error-404-ilustracion-concepto-paisaje_114360-7898.jpg?w=2000" alt="error 404" />
+            <p className="fs-1">NO FOUND</p>
         </Container>
     );
 };
